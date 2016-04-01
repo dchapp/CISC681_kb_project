b8b296970781ca403f30e97f177aa96326c73f59
dchapp <dchapp@udel.edu>
2016-04-01 15:53:09 -0400

    Added function for interactive building of knowledge base.

--- src/KB.py
+++ src/KB.py
@@ -6,19 +6,31 @@ import sympy as sp
 from Inference import *
 from TestCases import *
 from Utilities import *
+from MushroomFacts import *
 
-
+"""
+Generate a list of SymPy symbols representing facts in the knowledge base
+"""
 def parse_fact_file(fact_file_name):
+    
+    ### Read in facts 
     with open(fact_file_name) as fact_file:
         fact_list = []
-        for f in fact_file:
-            f = custom_strip(f)
-            fact_list.append(f)
+        for line in fact_file:
+            if line[0] == "#" or line[0] == "\t" or line[0] == " " or line == "\n":
+                continue
+            else:
+                fact_list.append(custom_strip(line))
 
-        print "FACT LIST:"
-        print fact_list
-        return fact_list
+    ### Format facts to generate Sympy-fiable strings
+    facts = []
+    for f in fact_list:
+        facts.append(custom_replace(f))
 
+    ### Transform to SymPy symbols
+    facts = [sp.sympify(x) for x in facts]
+
+    return facts
 
 """
 Generates a list of SymPy implications representing rules in the knowledge base.
@@ -29,8 +41,8 @@ def parse_rule_file(rule_file_name):
     with open(rule_file_name) as rule_file:
         rule_list = []
         for line in rule_file:
-            ### Ignore if line is a description or empty
-            if line[0] == "#" or line == "\n":
+            ### Ignore if line is a description, whitespace, or empty
+            if line[0] == "#" or line[0] == "\t" or line[0] == " " or line == "\n":
                 continue
             ### Otherwise add rule to list
             else:
@@ -40,7 +52,7 @@ def parse_rule_file(rule_file_name):
                 rule = (premises, conclusion)
                 rule_list.append(rule)
 
-    ### Format premises and conclusion properly and generate string
+    ### Format premises and conclusion to generate SymPy-fiable string
     rules = []
     for r in rule_list:
         premises = r[0]
@@ -56,10 +68,79 @@ def parse_rule_file(rule_file_name):
         rule_string = rule_string + ") >> " + conclusion
         rules.append(rule_string)
 
-    ### SymPy-fy
+    ### Transform to SymPy implications
     rules = [sp.sympify(x) for x in rules]
 
     return rules
+
+
+"""
+Constructs a knowledge base from a file of rules and a file of facts.
+The knowledge base is constructed as a CNF sentence suitable for use with the inference algorithms. 
+"""
+def build_knowledge_base(rule_file_name, fact_file_name):
+    rules = parse_rule_file(rule_file_name)
+    facts = parse_fact_file(fact_file_name)
+    kb    = rules + facts 
+    kb_sentence = kb[0]
+    for k in kb[1:]:
+        kb_sentence = kb_sentence & k
+    kb_cnf_sentence = sp.to_cnf(kb_sentence)
+    return kb_cnf_sentence
+
+
+"""
+Constructs a knowledge base from a file of rules and uses user input to establish a list of facts.
+The knowledge base is constructed as a CNF sentence suitable for use with the inference algorithms. 
+"""
+def build_knowledge_base_interactive(rule_file_name):
+    rules = parse_rule_file(rule_file_name)
+    (mushroom_properties, patient_properties) = define_properties()
+    fact_list = []
+    ### First iterate through list of mushroom properties and get user responses
+    print "Please answer the following questions about the mushroom the patient ingested."
+    print "If you do not know the answer, please enter \"?\""
+    for p in mushroom_properties:
+        if p == "stem has ring" or p == "stem has volva":
+            response = raw_input("Does the mushroom's " + p.replace("has", "have a") + "? ")
+            if response == "yes":
+                fact_list.append(p)
+            else:
+                continue
+        else:
+            response = raw_input("What is the mushroom's " + p + "? ")
+            if response == "?":
+                continue
+            else:
+                fact = p + " is " + response
+                fact_list.append(fact)
+
+    ### Next iterate through list of patient properties and get user responses
+    print "Please answer the following questions about the patient."
+    for p in patient_properties:
+        response = raw_input("Does the patient have " + p + "? ")
+        if response == "yes":
+            fact_list.append(p)
+        else:
+            continue
+
+    ### Format facts for conversion to SymPy symbols
+    facts = []
+    for f in fact_list:
+        facts.append(custom_replace(f))
+
+    ### Convert facts to SymPy symbols
+    facts = [sp.sympify(x) for x in facts]
+
+    ### Assemble KB
+    kb = rules + facts
+    kb_sentence = kb[0]
+    for k in kb[1:]:
+        kb_sentence = kb_sentence & k
+    kb_cnf_sentence = sp.to_cnf(kb_sentence)
+    return kb_cnf_sentence
+
+
 
 
 
@@ -111,7 +192,26 @@ def main():
     #parser.add_argument("-m", "--mode", nargs=1, default=[], help="How to process your query.")
     #args = parser.parse_args()
 
-    rules = parse_rule_file(sys.argv[1])
+
+    rule_file_name = sys.argv[1]
+    fact_file_name = sys.argv[2]
+
+    rules = parse_rule_file(rule_file_name)
+    facts_from_file = parse_fact_file(fact_file_name)
+    print "RULES:"
+    print rules
+    print "FACTS:"
+    print facts_from_file
+
+    print "TESTING KB BUILDER"
+    kb_from_file = build_knowledge_base(rule_file_name, fact_file_name)
+    print kb_from_file
+
+    print "TESTING INTERACTIVE KB BUILDER"
+    kb_interactive = build_knowledge_base_interactive(rule_file_name)
+    print kb_interactive
+
+    print "TESTING INFERENCE ALGORITHMS"
     run_inference_test_suite()
 
 main()
---
+++ src/MushroomFacts.py
@@ -0,0 +1,39 @@
+def define_properties():
+    mushroom_properties = ["gill color",
+                           "gill type",
+                           "cap shape",
+                           "cap color",
+                           "cap texture",
+                           "hymenium",
+                           "stem color",
+                           "stem texture",
+                           "stem has ring",
+                           "stem has volva",
+                           "spore print color",
+                           "odor",
+                           "location",
+                           "climate",
+                           "season",
+                           "host organism"]
+
+    patient_properties = ["heart rate > 100",
+                          "yellow skin or eyes",
+                          "upper abdominal pain",
+                          "lower abdominal pain",
+                          "nausea",
+                          "vomiting",
+                          "disorientation/confusion",
+                          "seizures",
+                          "myoclonus",
+                          "immense thirst",
+                          "frequent urination",
+                          "dilated pupils",
+                          "hallucinations",
+                          "headache",
+                          "diarrhea",
+                          "bloody diarrhea",
+                          "vertigo",
+                          "burning sensation in limbs"]
+
+    return (mushroom_properties, patient_properties)
+
